ff462f8f5782c253f12cdc56497aeb89fed688dd
Brice Petznick <bpetznick@gmail.com>
2023-10-12 12:40:43 -0500

    Updated Project file

--- index.js
+++ index.js
@@ -24,3 +24,14 @@ $(".tog1").click(function(){
     $('img',this).toggle();
     $('.skills2').toggle();
   });
+
+  
+$(document).ready(function() {
+
+  $('.gallery-item').hover(function() {
+    $(this).find('.img-title').fadeIn(300);
+  }, function() {
+    $(this).find('.img-title').fadeOut(100);
+  });
+
+});
