38cc8afc117865e0e6d99b1358bae0ac83bd5e3e
petznbr <bpetznick@gmail.com>
2023-10-11 21:26:09 -0500

    Added contact me page and redid site.

---
+++ index.js
@@ -0,0 +1,26 @@
+$(".menu").click(function(){$(".container").toggle();});
+
+$(".tog1").click(function(){
+    $('img',this).toggle();
+    $('.job1').toggle();
+  });
+
+  $(".tog2").click(function(){
+    $('img',this).toggle();
+    $('.job2').toggle();
+  });
+
+  $(".tog3").click(function(){
+    $('img',this).toggle();
+    $('.job3').toggle();
+  });
+
+  $(".tog4").click(function(){
+    $('img',this).toggle();
+    $('.skills1').toggle();
+  });
+
+  $(".tog5").click(function(){
+    $('img',this).toggle();
+    $('.skills2').toggle();
+  });
